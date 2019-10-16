853a5764ce8d4c1454b4363e81e72eee1e9faddc
Sahe Mayson <smayson94@gmail.com>
2019-10-15 20:27:06 -0400

    retrieve product images and add styling

--- cartApp.js
+++ cartApp.js
@@ -1,38 +1,79 @@
 
-
+	const products = [] || '';
+	let cartData;
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 
 
-	const insertCartRow = (prodcutPrice, productQuantity) => {
+	$.getJSON('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json').done(function(data){
+		cartData = data;
+		getCartProducts();
+		console.log(cartData);
+	});
+
+ //    // get product info from cart
+	// for (i = 0; i < cartData.cart.products.length; i++) {
+	// 	let product_id = cartData.cart.products[i].id;
+	// 	$.getJSON(`https://prodcat.gopuff.com/api/products?location_id=-1&product_id=${product_id}`).done(function(productData){
+	// 		products.push(productData);
+	// 	});
+	// }
+
+	const getCartProducts = () => {
+		for (i = 0; i < cartData.cart.products.length; i++) {
+			let product_id = cartData.cart.products[i].id;
+			$.getJSON(`https://prodcat.gopuff.com/api/products?location_id=-1&product_id=${product_id}`).done(function(productData){
+				products.push(productData);
+			});
+		}
+	}
+
+	const insertCartRow = (unitPrice, quantity, productImage, productTitle, productDescription) => {
 		var cartRow = document.createElement('div');
-		cartRow.setAttribute('class', 'post block bc2')
+		cartRow.setAttribute('class', 'item');
 		cartRow.innerHTML = `
-				<div class="cart-template-row col-sm-4 col-lg-12">
-				    <h2 class="product-title">TITLE</h2>
-				    <div class="panel panel-primary">
-				    	<div class="product-id">Product ID</div>
-				        <div class="product-price">${productPrice}</div>
-				        <div class="product-quantity">${productQuantity}</div>
-				  	</div>
-		    	</div>`;
+			    <div class="buttons">
+			      <span class="delete-btn"></span>
+			    </div>
+			 
+			    <div class="image">
+			      <img src="${productImage}" alt="" />
+			    </div>
+			 
+			    <div class="description">
+			      <span>${productTitle}</span>
+			      <span>${productDescription}</span>
+			    </div>
+			 
+			    <div class="quantity">
+			      <button class="plus-btn" type="button" name="button">
+			        <img src="plus.svg" alt="" />
+			      </button>
+			      <input type="text" name="name" value="${quantity}">
+			      <button class="minus-btn" type="button" name="button">
+			        <img src="" alt="" />
+			      </button>
+			    </div>
+			 
+			    <div class="total-price">${unitPrice}</div>`;
 		document.getElementById('cartContainer').appendChild(cartRow);
 	}
 
 	const initCartRow = () => {
-		let cartProducts
-		$.getJSON('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json', function(data) {
-		cartData = data;
-		console.log(cartData);
-		})
 
-		let quantitiy, unitPrice, totalPrice;
-		// for (i < 0; i < cartData.cart.products.length; i++) {
-		// 	quantitiy = cartData.cart.products[i].qunatity;
-		// 	unitPrice = cartData.cart.products[i].qunatity;
-		// }
+		let quantitiy, unitPrice, totalPrice, productImage;
+		for (i = 0; i < cartData.cart.products.length; i++) {
+				if (products.length){
+					quantity = cartData.cart.products[i].quantity;
+					unitPrice = cartData.cart.products[i].price;
+					productImage = products[i].products[0].avatar.small;
+					productTitle = products[i].products[0].name;
+					productDescription = products[i].products[0].description || "";
+					insertCartRow(unitPrice, quantity, productImage, productTitle, productDescription);
+				}
+		}
 	}
 
-	initCartRow()
+	setTimeout(initCartRow, 3000);
 });
 
