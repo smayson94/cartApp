8d03b04639047b990e3802dc4f91ed649e22649a
Sahe Mayson <smayson94@gmail.com>
2019-10-15 21:31:45 -0400

    add styling and buttons, calculate total

--- cartApp.js
+++ cartApp.js
@@ -1,33 +1,32 @@
 
-	const products = [] || '';
-	let cartData;
+const products = [] || '';
+let cartData;
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-
-	$.getJSON('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json').done(function(data){
+document.addEventListener("DOMContentLoaded", (event) => { 
+	// get product order JSON
+	$.getJSON('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json').done((data)=>{
 		cartData = data;
 		getCartProducts();
 		console.log(cartData);
 	});
 
- //    // get product info from cart
-	// for (i = 0; i < cartData.cart.products.length; i++) {
-	// 	let product_id = cartData.cart.products[i].id;
-	// 	$.getJSON(`https://prodcat.gopuff.com/api/products?location_id=-1&product_id=${product_id}`).done(function(productData){
-	// 		products.push(productData);
-	// 	});
-	// }
-
+	// get product info from Cart with goPuff API
 	const getCartProducts = () => {
 		for (i = 0; i < cartData.cart.products.length; i++) {
 			let product_id = cartData.cart.products[i].id;
-			$.getJSON(`https://prodcat.gopuff.com/api/products?location_id=-1&product_id=${product_id}`).done(function(productData){
+			$.getJSON(`https://prodcat.gopuff.com/api/products?location_id=-1&product_id=${product_id}`)
+					.done((productData) =>{
 				products.push(productData);
 			});
 		}
 	}
 
+	// const bindCartButtons = () => {
+	// 	let deleteButtons = $('delete-btn');
+	// 	for (i=0; i < deleteButtons.length; i++) {
+	// }
+
+	// insert cart Row for Each Product
 	const insertCartRow = (unitPrice, quantity, productImage, productTitle, productDescription) => {
 		var cartRow = document.createElement('div');
 		cartRow.setAttribute('class', 'item');
@@ -37,31 +36,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			    </div>
 			 
 			    <div class="image">
-			      <img src="${productImage}" alt="" />
+			      <img src="${productImage}"/>
 			    </div>
 			 
 			    <div class="description">
 			      <span>${productTitle}</span>
-			      <span>${productDescription}</span>
 			    </div>
-			 
+			
 			    <div class="quantity">
 			      <button class="plus-btn" type="button" name="button">
-			        <img src="plus.svg" alt="" />
+			        <img src="plus.svg"/>
 			      </button>
 			      <input type="text" name="name" value="${quantity}">
 			      <button class="minus-btn" type="button" name="button">
-			        <img src="" alt="" />
+			        <img src="minus.svg"/>
 			      </button>
 			    </div>
 			 
 			    <div class="total-price">${unitPrice}</div>`;
-		document.getElementById('cartContainer').appendChild(cartRow);
+		$('#cartContainer')[0].appendChild(cartRow);
 	}
 
-	const initCartRow = () => {
-
+	// build Cart
+	const initCart = () => {
 		let quantitiy, unitPrice, totalPrice, productImage;
+		let priceCounter = 0;
 		for (i = 0; i < cartData.cart.products.length; i++) {
 				if (products.length){
 					quantity = cartData.cart.products[i].quantity;
@@ -70,10 +69,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 					productTitle = products[i].products[0].name;
 					productDescription = products[i].products[0].description || "";
 					insertCartRow(unitPrice, quantity, productImage, productTitle, productDescription);
+					totalPrice = priceCounter + (unitPrice * quantity);
+					priceCounter = totalPrice;
 				}
 		}
+		$('#totalPrice')[0].innerHTML = "Total: " + priceCounter.toFixed(2);
 	}
 
-	setTimeout(initCartRow, 3000);
+	setTimeout(initCart, 2000);
+
 });
 
